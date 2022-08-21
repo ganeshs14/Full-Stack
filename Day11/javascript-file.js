b407d0c56d3c3cbd9c839d85a5ce56fdb20bbbc5
ganeshs14 <ganeshs.0520@gmail.com>
2022-08-21 10:51:25 +0530

    day13

---
+++ Day11/javascript-file.js
@@ -0,0 +1,53 @@
+// console.log("Hello, this is a Javascript code.");
+// single line comment
+
+/*
+multi
+line
+comment
+*/
+
+var name="Ganesh";
+var year="2022";
+console.log(name,"\n", year);
+
+var name="Ganesh Shelar";
+var year="2022-23";
+console.log(name,"\n", year);
+
+const var_name = "John Doe";
+console.log(var_name);
+
+const bool=true;
+console.log(bool);
+
+var one=1;
+var two=2.25;
+console.log(one);
+console.log(two);
+
+
+// arrays
+
+var arr = [1,2,3,4,5];
+console.log(arr);
+
+var arr1 = [1, "one", 2.67, true, undefined, null]
+console.log(arr1);
+
+
+console.log(arr1[1]);
+console.log(arr1[-2]);
+
+// objects
+
+var obj = {
+  //key : value;
+  id : 101,
+  name : "First Name",
+  marks : [90, 79, 45]
+}
+
+console.log(obj);
+
+console.log(obj.name);
