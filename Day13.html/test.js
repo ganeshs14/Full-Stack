32ddf63e4cacf19f1d8246b83225a2824d339ccb
ganeshs14 <ganeshs.0520@gmail.com>
2022-08-28 18:40:15 +0530

    day15

---
+++ Day13.html/test.js
@@ -0,0 +1,23 @@
+// Conditional Statements
+// 1. IF ELSE
+// 2. SWITCH
+
+// if().. ,
+// if().. else().. ,
+// if().. else if().. else().. ;
+
+var n1 = 10;
+var n2 = 50;
+// var n2 = 10;
+
+console.log("n1 :", n1, "\nn2 :", n2);
+
+if(n1>n2) {
+  console.log("n1 is greater than n2");
+}
+else if(n2>n1) {
+  console.log("n2 is greater than n1");
+}
+else {
+  console.log("n1 is equal to n2");
+}
